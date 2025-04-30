2dc21b984b8c619fefecc7d0d9f9bacfee75b544
S-Fennell <67168523+S-Fennell@users.noreply.github.com>
2025-04-30 15:44:24 -0400

    prime done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// //Fizz Buzz
 for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
       console.log("Fizz Buzz");
@@ -10,3 +10,31 @@ for (let i = 1; i <= 100; i++) {
       console.log(i);
     }
   }
+
+//Prime Time
+let n = 21;
+
+while(true){
+    let prime = true;
+    for(let i = 2; i < n; i++){
+        if(n % i === 0){
+            prime = false;
+            break;
+        }
+    }
+   
+    if(prime){
+        console.log(n + " is prime");
+        break;
+    }
+    n++ 
+}
+
+
+
+
+
+
+
+
+
