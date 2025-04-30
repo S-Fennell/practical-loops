33476feb96e88fbe7ac5bd933bc0369fa6fedfbd
S-Fennell <67168523+S-Fennell@users.noreply.github.com>
2025-04-30 15:59:39 -0400

    Henry,Kevitha,David,Chanel finished

--- script.js
+++ script.js
@@ -29,8 +29,63 @@ while(true){
     }
     n++ 
 }
+//Feeling Loopy
 
+let data ="ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26"
+let key =1;
 
+let cell1 = "";
+let cell2 = "";
+let cell3 = "";
+let cell4 = "";
+
+for(let i = 0; i < data.length; i++){
+   
+    switch (key) {
+        case 1:
+            if(data[i] === ","){
+                key = 2;
+            }else{
+                cell1 += data[i];
+            }
+        
+            break;
+
+        case 2:
+            if(data[i] === ","){
+                key = 3;
+            }else{
+                cell2 += data[i];
+            }
+        
+            break;
+        case 3:
+        if(data[i] === ","){
+            key = 4;
+        }else{
+            cell3 += data[i];
+        }
+    
+        break;
+        case 4:
+            if(data[i] === "\n" || i === data.length - 1){
+                key = 1;
+                console.log(cell1, cell2, cell3, cell4);
+                cell1 = "";
+                cell2 = "";
+                cell3 = "";
+                cell4 = "";
+
+            }else{
+                cell4 += data[i];
+            }
+        
+            break;
+    
+        default:
+            break;
+    }
+}
 
 
 
